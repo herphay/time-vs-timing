4a165be5a39f29282ed71c29821622281b90b579
herphay <herphay@gmail.com>
2025-04-30 22:12:51 +0800

    Move converting col data to numpy arrays into dedicated numpyfy func

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from collections.abc import Iterator
 from data import pull_ticker_data
 
 def main() -> None:
@@ -17,24 +18,23 @@ def plot_day_range(ticker: str, start: str = None, end: str = None) -> None:
     bins = 21
 
     # Obtain historical data used in plotting
-    data = pull_ticker_data(ticker, cols='date, high, low', start=start, end=end)
-    dates, highs, lows = zip(*data) # Convert list of tuples (of rows) into list of tuples (of columns)
-    dates = np.array(dates)
-    highs = np.array(highs)
-    lows = np.array(lows)
+    cols = ('date', 'high', 'low')
+    raw_data = pull_ticker_data(ticker, cols=', '.join(cols), start=start, end=end)
+    # zip(*raw_data) converts list of tuples (of rows) into list of tuples (of columns)
+    data_dict = numpyfy_data(zip(cols, zip(*raw_data)))
 
     fig, ax = plt.subplots()
-    ax.fill_between(dates, highs, lows, alpha=.5, linewidth=0)
+    ax.fill_between(data_dict['date'], data_dict['high'], data_dict['low'], alpha=.5, linewidth=0)
     # ax.plot(dates, (highs + lows)/2, linewidth=2)
 
     ### Manual tick location setting ###
     # Manual calc (round to int)
-    xidx = get_date_idx(len(dates), bins)
+    xidx = get_date_idx(len(data_dict['date']), bins)
     
     # Using Numpy linspace (round down to int)
     # x_idx2 = np.array(np.linspace(0, len(dates)-1, 21, dtype=int))
     
-    ax.set_xticks(xidx, dates[xidx], rotation=45, ha='right')
+    ax.set_xticks(xidx, data_dict['date'][xidx], rotation=45, ha='right')
     # ax.tick_params('x', rotation=90)   # redundant, another way to set rotation
     # fig.autofmt_xdate()                # Auto rotate dates but does not properly increase tick gap
 
@@ -51,32 +51,39 @@ def plot_composite(ticker: str, start: str = None, end: str = None) -> None:
     """
     bins = 21
 
-    data = pull_ticker_data(ticker, cols='date, high, low, open, close', start=start, end=end)
-    dates, highs, lows, opens, closes = zip(*data)
-    dates  = np.array(dates)
-    highs  = np.array(highs)
-    lows   = np.array(lows)
+    cols = ('date', 'high', 'low', 'open', 'close')
+    raw_data = pull_ticker_data(ticker, cols=', '.join(cols), start=start, end=end)
+    data_dict = numpyfy_data(zip(cols, zip(*raw_data)))
     # Combine open and close data into 1 2-dim array
     # transpose to make it Nx2 array where N is num of dates
     # Essentially make each row correspond to a date, and 1st col is open, 2nd is close
-    opclo  = np.array([opens, closes]).T
+    opclo  = np.array([data_dict['open'], data_dict['close']]).T
 
     fig, ax = plt.subplots()
 
-    ax.fill_between(dates, highs, lows, alpha=0.5, linewidth=0)
+    ax.fill_between(data_dict['date'], data_dict['high'], data_dict['low'], alpha=0.5, linewidth=0)
 
     # plotting a NxM array in y-axis means plotting M lines with N datapoints each
-    ax.plot(dates, opclo, label=['open', 'close'])
+    ax.plot(data_dict['date'], opclo, label=['open', 'close'])
 
-    xidx = get_date_idx(len(dates), bins)
+    xidx = get_date_idx(len(data_dict['date']), bins)
 
-    ax.set_xticks(xidx, dates[xidx], rotation=45, ha='right')
+    ax.set_xticks(xidx, data_dict['date'][xidx], rotation=45, ha='right')
 
     set_ax_labels(ax, f'Daily price range and open/close prices against time for: {ticker}')
     ax.legend() # to show legend for lines
 
     # plt.show(block=False)
 
+
+def numpyfy_data(col_datas: Iterator[tuple[str, tuple]]) -> dict:
+    """
+    col_datas: zip object
+        Expects a zip object combining column name with the corresponding time series
+    """
+    return {col_name: np.array(col_data)
+            for col_name, col_data
+            in col_datas}
 
 def get_date_idx(len: int, bins: int) -> list:
     bins = min(bins, len)
