ac65db588865bc76a5f90c36c60b1cee54c04d46
herphay <herphay@gmail.com>
2025-05-01 14:55:50 +0800

    Added option to utilize matplotlib auto date ticker functionality

--- plot.py
+++ plot.py
@@ -1,29 +1,33 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from collections.abc import Iterator
+
 from data import pull_ticker_data
 
 # Global data definition
 bins = 21 # Number of x-axis ticks to display for charts
 
 def main() -> None:
-    plot_day_range('VT', start='2025-01-01')
-    plot_composite('VT', start='2025-01-01')
+    plot_day_range('VT', start='2025-01-01', autodate=True)
+    plot_composite('VT', start='2025-01-01', autodate=False)
     plt.show() # plt.show() only outside to ensure all plots can show together
 
 
-def plot_day_range(ticker: str, start: str = None, end: str = None) -> None:
+def plot_day_range(ticker: str, start: str | None = None, end: str | None = None, autodate: bool = True) -> None:
     """
     ticker: str
     start/end: str
         ISO 8601 (YYYY-MM-DD)
+    autodate: bool
+        Whether we use matplotlib default date locator
     """
 
     # Obtain historical data used in plotting
     cols = ('date', 'high', 'low')
-    data = get_plot_data(ticker, cols, start=start, end=end)
+    data = get_plot_data(ticker, cols, start=start, end=end, autodate=autodate)
 
-    ax = setup_plot_elements(data['date'], f'Daily price range against time for: {ticker}')
+    ax = setup_plot_elements(data['date'], f'Daily price range against time for: {ticker}', autodate=autodate)
     
     ax.fill_between(data['date'], data['high'], data['low'], alpha=.5, linewidth=0)
     # ax.plot(dates, (highs + lows)/2, linewidth=2)
@@ -31,29 +35,31 @@ def plot_day_range(ticker: str, start: str = None, end: str = None) -> None:
     # plt.show(block=False) # For interactive mode but the moment the local function context ends, plot will also close
 
 
-def plot_composite(ticker: str, start: str = None, end: str = None) -> None:
+def plot_composite(ticker: str, start: str | None = None, end: str | None = None, autodate: bool = True) -> None:
     """
     ticker: str
     start/end: str
         ISO 8601 (YYYY-MM-DD)
+    autodate: bool
+        Whether we use matplotlib default date locator
     """
 
     cols = ('date', 'high', 'low', 'open', 'close')
-    data = get_plot_data(ticker, cols, start=start, end=end)
+    data = get_plot_data(ticker, cols, start=start, end=end, autodate=autodate)
 
     # Combine open and close data into 1 2-dim array
     # transpose to make it Nx2 array where N is num of dates
     # Essentially make each row correspond to a date, and 1st col is open, 2nd is close
     opclo  = np.stack([data['open'], data['close']]).T
 
-    ax = setup_plot_elements(data['date'], f'Daily price range and open/close prices against time for: {ticker}')
+    ax = setup_plot_elements(data['date'], f'Daily price range and open/close prices against time for: {ticker}', autodate=autodate)
 
     ax.fill_between(data['date'], data['high'], data['low'], alpha=0.5, linewidth=0)
 
     # plotting a NxM array in y-axis means plotting M lines with N datapoints each
     ax.plot(data['date'], opclo, label=['open', 'close'])
 
-    ax.legend() # to show legend for lines
+    ax.legend() # to show legend for lines; Must be called after line's legent are defined through label=labels
 
     # plt.show(block=False)
 
@@ -61,7 +67,8 @@ def plot_composite(ticker: str, start: str = None, end: str = None) -> None:
 def get_plot_data(ticker: str, 
                   cols: tuple[str], 
                   start: str | None = None, 
-                  end: str | None = None) -> dict[str, np.ndarray]:
+                  end: str | None = None,
+                  autodate: bool = True) -> dict[str, np.ndarray]:
     """
     Pull the required plot data & process it into a dict of Numpy Arrays for each column
 
@@ -80,7 +87,13 @@ def get_plot_data(ticker: str,
     raw_data = zip(*raw_data)         # Convert row data to col data
     packed_data = zip(cols, raw_data) # Attach col name to each col data
 
-    return {col_name: np.array(col_data) for col_name, col_data in packed_data}
+    packed_data = {col_name: np.array(col_data) for col_name, col_data in packed_data}
+    if autodate:
+        # specify Numpy datetime64 dtype, with millisecond precision [ms] rather than day [D]
+        # Reason being we need ms precision for the datetime64 object to be successfully converted
+        # to standard python datetime object later in setup_plot_elements section with .item() method 
+        packed_data['date'] = np.array(packed_data['date'], dtype='datetime64[ms]')
+    return packed_data
 
 ##### No longer required after re-factoring all data processing to dedicated get_plot_data func #####
 # def numpyfy_data(col_datas: Iterator[tuple[str, tuple]]) -> dict[str, np.ndarray]:
@@ -103,7 +116,8 @@ def get_date_idx(length: int, bins: int) -> list[int]:
 def setup_plot_elements(dates: np.array,
                         title: str,
                         xlabel: str = 'Dates', 
-                        ylabel: str = 'Price') -> plt.Axes:
+                        ylabel: str = 'Price',
+                        autodate: bool = True) -> plt.Axes:
     """
     Set up a plot and all basic elements including tick locations
 
@@ -111,22 +125,51 @@ def setup_plot_elements(dates: np.array,
         Numpy array of dates used in the plot   
     title: str    
         Title of the chart  
+    autodate: bool
+        Determine whether x-axis date tickers are set up by matplotlib auto locator    
+        OR
+        manual calculation of bins amount of equally spaced ticks
+        Both will always include the start & end date
     """
     fig, ax = plt.subplots()
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
 
-    ### Manual tick location setting ###
-    # Manual calc (round to int)
-    xidx = get_date_idx(len(dates), bins)
-    
-    # Using Numpy linspace (round down to int)
-    # x_idx2 = np.array(np.linspace(0, len(dates)-1, 21, dtype=int))
+    if autodate and len(dates) > 1:
+        # modify start/end date to matplotlib specific dates numbers
+        start_date_num = mdates.date2num(dates[0])
+        end_date_num   = mdates.date2num(dates[-1])
 
-    ax.set_xticks(xidx, dates[xidx], rotation=45, ha='right')
-    # ax.tick_params('x', rotation=90)   # redundant, another way to set rotation
-    # fig.autofmt_xdate()                # Auto rotate dates but does not properly increase tick gap
+        # Set x-axis limits to ensure chart fill up the entire axis
+        # also critical to help AutoDateLocator to locate proper dates 
+        ax.set_xlim(dates[0], dates[-1])
+
+        # Get the auto-calculated tick locations
+        # For some reason, calling the tick_values method directly like this require
+        # us to convert the input date values as python datetime dtype, else it will be a TypeError
+        # Numpy datetime64 objects can be converted to python datetime objects with .item() method
+        # However, the datetime64 object must have at least ms precision -> datetime64[ms]
+        auto_ticks = mdates.AutoDateLocator().tick_values(dates[0].item(), dates[-1].item())
+
+        # To ensure both the start & end dates are included in the ticks
+        # we will join the auto calculated ticks with the start/end date ticks
+        # np.unique then return the unique sorted ticks to be set as the x-axis
+        all_ticks = np.unique(np.concat((auto_ticks, (start_date_num, end_date_num))))
+        ax.set_xticks(all_ticks)
+        fig.autofmt_xdate(rotation=45, ha='right')
+    else:
+        ##### Manual tick location setting #####
+        ax.set_xlim(0, len(dates) - 1)
+        # Manual calc (round to int)
+        xidx = get_date_idx(len(dates), bins)
+        
+        # Using Numpy linspace (round down to int)
+        # x_idx2 = np.array(np.linspace(0, len(dates)-1, 21, dtype=int))
+
+        ax.set_xticks(xidx, dates[xidx], rotation=45, ha='right')
+        # ax.tick_params('x', rotation=90)   # redundant, another way to set rotation
+        # fig.autofmt_xdate()                # Auto rotate dates but does not properly increase tick gap
 
     return ax
 
