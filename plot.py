7a90ed0f00b494682fc6c0a20d0794089bc3ec16
herphay <herphay@gmail.com>
2025-05-01 08:46:33 +0800

    Moved all common plot set up functionality to dedicated func; Updated docstrings

--- plot.py
+++ plot.py
@@ -28,24 +28,12 @@ def plot_day_range(ticker: str, start: str = None, end: str = None) -> None:
     # zip(*raw_data) converts list of tuples (of rows) into list of tuples (of columns)
     data_dict = numpyfy_data(zip(cols, zip(*raw_data)))
 
-    fig, ax = plt.subplots()
+    ax = setup_plot_elements(data_dict['date'], f'Daily price range against time for: {ticker}')
+    
     ax.fill_between(data_dict['date'], data_dict['high'], data_dict['low'], alpha=.5, linewidth=0)
     # ax.plot(dates, (highs + lows)/2, linewidth=2)
 
-    ### Manual tick location setting ###
-    # Manual calc (round to int)
-    xidx = get_date_idx(len(data_dict['date']), bins)
-    
-    # Using Numpy linspace (round down to int)
-    # x_idx2 = np.array(np.linspace(0, len(dates)-1, 21, dtype=int))
-    
-    ax.set_xticks(xidx, data_dict['date'][xidx], rotation=45, ha='right')
-    # ax.tick_params('x', rotation=90)   # redundant, another way to set rotation
-    # fig.autofmt_xdate()                # Auto rotate dates but does not properly increase tick gap
-
-    set_ax_labels(ax, f'Daily price range against time for: {ticker}')
-
-    # plt.show(block=False)
+    # plt.show(block=False) # For interactive mode but the moment the local function context ends, plot will also close
 
 
 def plot_composite(ticker: str, start: str = None, end: str = None) -> None:
@@ -66,18 +54,13 @@ def plot_composite(ticker: str, start: str = None, end: str = None) -> None:
     # Essentially make each row correspond to a date, and 1st col is open, 2nd is close
     opclo  = np.array([data_dict['open'], data_dict['close']]).T
 
-    fig, ax = plt.subplots()
+    ax = setup_plot_elements(data_dict['date'], f'Daily price range and open/close prices against time for: {ticker}')
 
     ax.fill_between(data_dict['date'], data_dict['high'], data_dict['low'], alpha=0.5, linewidth=0)
 
     # plotting a NxM array in y-axis means plotting M lines with N datapoints each
     ax.plot(data_dict['date'], opclo, label=['open', 'close'])
 
-    xidx = get_date_idx(len(data_dict['date']), bins)
-
-    ax.set_xticks(xidx, data_dict['date'][xidx], rotation=45, ha='right')
-
-    set_ax_labels(ax, f'Daily price range and open/close prices against time for: {ticker}')
     ax.legend() # to show legend for lines
 
     # plt.show(block=False)
@@ -100,13 +83,35 @@ def get_date_idx(length: int, bins: int) -> list[int]:
     return [round(pos * (length - 1) / (bins - 1)) for pos in range(bins - 1)] + [length - 1]
 
 
-def set_ax_labels(ax: plt.Axes, 
-                  title: str, 
-                  xlabel: str = 'Dates', 
-                  ylabel: str = 'Price') -> None:
+def setup_plot_elements(dates: np.array,
+                        title: str,
+                        xlabel: str = 'Dates', 
+                        ylabel: str = 'Price') -> plt.Axes:
+    """
+    Set up a plot and all basic elements including tick locations
+
+    dates: np.array    
+        Numpy array of dates used in the plot   
+    title: str    
+        Title of the chart  
+    """
+    fig, ax = plt.subplots()
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
+
+    ### Manual tick location setting ###
+    # Manual calc (round to int)
+    xidx = get_date_idx(len(dates), bins)
+    
+    # Using Numpy linspace (round down to int)
+    # x_idx2 = np.array(np.linspace(0, len(dates)-1, 21, dtype=int))
+
+    ax.set_xticks(xidx, dates[xidx], rotation=45, ha='right')
+    # ax.tick_params('x', rotation=90)   # redundant, another way to set rotation
+    # fig.autofmt_xdate()                # Auto rotate dates but does not properly increase tick gap
+
+    return ax
 
 
 if __name__ == '__main__':
